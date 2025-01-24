e321c2fe2d476fd7ec941550de0a5884633fdad4
sabayohannes <makidaqueens2@gmail.com>
2025-01-24 15:58:20 +0300

    god help

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ export default {
   ],
   theme: {
     extend: {},
-    colors:{'primary':"#5f6FFF"}
+    colors:{'primary':"#5f6FFF"},
+    gridTemplateColumns:{
+      'auto':'repeat(auto-fill,minmax(200px, 1fr))'
+    }
   },
+
   plugins: [],
 }
