df804122fb0f78461a53d4aa3394699fbab47d97
saba <makidaqueens2@gmail.com>
2025-01-26 12:50:56 +0300

    thanks god

--- src/pages/Appointment.jsx
+++ src/pages/Appointment.jsx
@@ -17,18 +17,24 @@ useEffect(()=>(
 
   return docInfo && (
     <div>
-      <div>
+      <div className='flex flex-cols sm:flex-row gap-4'>
         <div>
-          <img src={docInfo.image} alt=''/>
+          <img  className='bg-primary w-full sm:mas-w-72 rounded-lg' src={docInfo.image} alt=''/>
         </div>
 
-      </div>
-      <p>{docInfo.name} <img src={assets.verified_icon} alt=''/></p>
-      <div>
+        <div className='flex-1 border border-gray-400 rounded-lg p-8 py-7 bg-white mx-2 sm:mx-0 mt-[-80px] sm:mt-0'>
+      
+      <p className='flex items-center gap-2 text-2xl font-medium text-gray-900'>{docInfo.name} <img  className='w-5' src={assets.verified_icon} alt=''/></p>
+ <div className='flex items-center gap-2 text-sm mt-1 text-gray-600'>   
       <p> {docInfo.degree}-{docInfo.speciality}</p>
     
-      <button>{docInfo.experience}</button></div>
-      <p></p>
+      <button className='py-0.5 px-2 border text-xs rounded-full'>{docInfo.experience}</button>
+      <div> <p>About <img src={assets.info_icon} alt=''/></p>
+      <p>{docInfo.about}</p>
+      </div>  
+      </div>
+      </div>
+      </div>
     </div>
   )
 }
