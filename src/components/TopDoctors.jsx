be139cac90b49cf26c2a64992f12551c98ffb7bc
saba <makidaqueens2@gmail.com>
2025-01-30 22:03:43 +0300

    luv

--- src/components/TopDoctors.jsx
+++ src/components/TopDoctors.jsx
@@ -1,7 +1,6 @@
 import React, { useContext } from 'react'
 
 import { useNavigate } from 'react-router-dom'
-import Appointment from '../pages/Appointment';
 import {AppContext} from '../context/AppContext';
 
 
@@ -16,7 +15,7 @@ const TopDoctors = () => {
       </div>
       <div className='w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0'>
         {doctors.slice(0, 10).map((item, index) => (
-          <div onClick={()=>{navigate(`/appointment/${item._id}`);scrollTo(0,0)}}
+          <div onClick={()=>navigate(`/appointment/${item._id}`)}
             className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500'
             key={index}
           >
